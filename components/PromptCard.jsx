96f73a43f38efc121830b48bbdb5ab3ab3b64569
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-02-18 02:57:11 +0600

    Add components

---
+++ components/PromptCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PromptCard = () => {
+  return <div>promtcard</div>;
+};
+
+export default PromptCard;
---
+++ components/Provider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Provider = () => {
+  return <div>profider</div>;
+};
+
+export default Provider;
