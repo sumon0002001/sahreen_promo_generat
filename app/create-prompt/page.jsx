efb3c41a383db031fc4a53e51899d535349d5af0
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-02-22 05:30:34 +0600

    Stuck in bring all post to home page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -3,20 +3,21 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setIsSubmitting(true);
-
+    setSubmitting(true);
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
@@ -26,25 +27,28 @@ const CreatePrompt = () => {
           tag: post.tag,
         }),
       });
-
       if (response.ok) {
         router.push("/");
       }
     } catch (error) {
-      console.log(error);
+      console.log("====================================");
+      console.log("erroe");
+      console.log("====================================");
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
 
   return (
-    <Form
-      type="Create"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
+    <div>
+      <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+      />
+    </div>
   );
 };
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout ">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const handleTagClick = (tag) => {};
 
 const Feed = () => {
-  return <div>feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [allPosts, setAllPosts] = useState([]);
+
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    // Fetch posts from the server or API
+    const fetchPrompts = async () => {
+      const response = await fetch("/api/prompts");
+      const data = await response.json();
+
+      setAllPosts(data);
+    };
+    fetchPrompts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center ">
+        <input
+          type="text"
+          placeholder="Search for a tag or r username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+          required
+        />
+      </form>
+      <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+    </section>
+  );
 };
 
 export default Feed;
