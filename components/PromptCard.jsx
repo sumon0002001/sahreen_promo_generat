e331052a576f1c1a16d1418f8e0fc1f8bda9d666
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-02-22 18:33:30 +0600

    Add promptcard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = () => {
-  return <div>promtcard</div>;
+const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div>
+          <Image
+            src={prompt.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {prompt.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {prompt.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={() => {}}>
+          <Image
+            src={
+              copied === prompt.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt={copied === prompt.prompt ? "tick" : "copy"}
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;
